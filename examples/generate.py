4cbd4958ab3de869ece96e6e6a824450c22a2c21
Dan McArdle <zingermc@gmail.com>
2013-01-27 01:10:15 -0500

    parsing octave and note names from input to function

--- examples/generate.py
+++ examples/generate.py
@@ -3,20 +3,29 @@ This program will be used to generate audio test files. These test files will
 be read in by the sheet music writer.
 """
 import math
+import re
 
 class Note:
     """This class will be used for finding the frequency associated with a note
     name"""
     
-    A1_FREQUENCY = 55
+    A0_FREQUENCY = 27.5
 
     def __init__(self):
         pass 
 
     def getFreq( noteName ):
         """Parse the noteName param according to Scientific Pitch Notation and give the frequency"""
-        return 0
 
+        # find octave and note name
+        octaveNum = re.findall("\d+", noteName)
+        noteName = re.findall("(A|B|C|D|E|F|G)(#|b|)", noteName) 
+
+        # calculate half steps from our base A0 frequency
+        
+        # determine frequency based on number of half steps
+
+        return 0
 
 def writeAudioFile(melody):
     for noteName in melody:
