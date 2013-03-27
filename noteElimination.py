b810920a10d89df4bd0973510895ac73ff6251e6
aah8 <aah8@geneseo.edu>
2013-03-27 17:15:49 -0300

    Create noteElimination.py
    Changed the MATLAB NoteElimination file into Python (I think :) Still have a limited knowledge of Python...)

---
+++ noteElimination.py
@@ -0,0 +1,34 @@
+def noteElimination(N, D):
+
+sum = 0;
+i = 1;
+
+for x in range(1, len(N)):
+  sum = sum + N(x)
+
+minAvgNoteEnergy = sum/len(N)
+
+for y in range(1, len(N))
+	if (N(j) < minAvgNoteEnergy):
+		E(i) = N(y)
+	else:
+		C(i) = N(y)
+		i=i+1
+
+
+sort(D)
+minTrajLen = D(1)
+
+for j in range(1, len(N))
+	if D(j) < minTrajLen:
+		E(end+1) = N(j)
+	else:
+		C(end+1) = N(j)
+		i=i+1
+
+sort(N)
+for z in range(1, len(N))
+	for a in range(z+1, len(N))
+		if (N(j) < N(i)*.5:
+			C(end+1) = N(z)
+			E(end+1) = N(z)
