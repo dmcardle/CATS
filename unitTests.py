aea103e5724a5e60c2503cd217bdefa3e6c0e051
Dan McArdle <zingermc@gmail.com>
2013-04-11 20:51:30 -0400

    tabs -> spaces

--- unitTests.py
+++ unitTests.py
@@ -18,10 +18,10 @@ class TestExampleGenerator(unittest.TestCase):
         f = int(n.freq + 0.5)
         self.assertEqual(f, 440)
 		
-	def testNoteElim(self):
-		t = transcriber.Transcriber()
-		e = t.noteElimination([220, 440, 450, 500, 550], [.5, 1, .7, .8, .4])
-		self.assertEqual(e, [440])
+    def testNoteElim(self):
+        t = transcriber.Transcriber()
+        e = t.noteElimination([220, 440, 450, 500, 550], [.5, 1, .7, .8, .4])
+        self.assertEqual(e, [440])
 
 if __name__ == '__main__':
     unittest.main()
